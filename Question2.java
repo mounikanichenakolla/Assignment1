4e7c5007a824f87444921a63773d443f8514d3be
mounikanichenakolla <115568218+mounikanichenakolla@users.noreply.github.com>
2022-10-24 16:32:31 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,15 @@
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+    for(int i=1;i<n;i++) {
+	for(int j=0;j<n;j++) {
+		System.out.print(i);
+}
+	System.out.println();
+	}
+
+}
+}
---
+++ Question4.java
@@ -0,0 +1,22 @@
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=14;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if((i>(3*n)/2 && i<n-1)||i-j>=(n-1)/2 || i+j>=(n-1)+(n-1)/2){
+					System.out.print("*");
+				}
+				else {
+				System.out.print(" ");
+			}
+			
+		}
+		System.out.println();
+	}
+	}
+}
+
+	
